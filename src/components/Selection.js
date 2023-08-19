21c7d3f6f9b4ec10abdab1f648ebb0fd067809d4
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-08-19 17:36:10 +0000

    1st

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
 
+import React, { useState } from 'react';
+
+const Selection = (props) => {
+  const [selectionStyle, updateSelectionStyle] = useState({ background: '' });
+
+  const { applyColor } = props;
+
+  return (
+    <div
+      className="fix-box"
+      style={selectionStyle}
+      onClick={() => applyColor(updateSelectionStyle)}
+    ></div>
+  );
+};
+
+export default Selection;
